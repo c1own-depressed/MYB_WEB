51a4a94ab40ea32a1ab8f3a948565f9925982ab4
Javenhun733r <andrij.babyuk@gmail.com>
2024-03-14 13:38:20 +0200

    Added StyleCop and renewed 2 files

--- ManageYourBudget/WebApp/Controllers/HomeController.cs
+++ ManageYourBudget/WebApp/Controllers/HomeController.cs
@@ -1,23 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Serilog;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace WebApp.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Controller for managing home page.
+    /// </summary>
     public class HomeController : Controller
     {
-        private readonly LogController _logController;
+        private readonly LogController logController;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// Constructor for HomeController.
+        /// </summary>
+        /// <param name="logController">The log controller instance.</param>
         public HomeController(LogController logController)
         {
-            _logController = logController;
+            this.logController = logController;
         }
 
+        /// <summary>
+        /// Action method for rendering the home page.
+        /// </summary>
+        /// <returns>ViewResult representing the home page view.</returns>
         public IActionResult Index()
         {
-            // Виклик методу контролера LogControllers, який записує логи
-            _logController.Index();
+            // Call the method of LogController which logs.
+            this.logController.Index();
 
-            return View();
+            return this.View();
         }
     }
 }
