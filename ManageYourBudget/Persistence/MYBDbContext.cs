8ffb4a4866ca03de7a5b1c25f6c19989fa950b4c
OKmeray <yar.rom.rom@gmail.com>
2024-03-15 09:25:09 +0200

    Corrected EF connection

--- ManageYourBudget/Persistence/MYBDbContext.cs
+++ ManageYourBudget/Persistence/MYBDbContext.cs
@@ -1,7 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
 using Domain.Entities;
 
 namespace Persistence.Data
@@ -9,26 +6,12 @@ namespace Persistence.Data
     public class MYBDbContext : DbContext
     {
         public MYBDbContext(DbContextOptions<MYBDbContext> options) : base(options) { }
-        public MYBDbContext() { }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                var connectionString = "server=localhost;port=3306;database=mybdatabase;user=root;password=1q2w3e4r";
-                optionsBuilder.UseMySQL(connectionString);
-            }
-        }
-
 
         public DbSet<User> Users { get; set; }
         public DbSet<Savings> Savings { get; set; }
         public DbSet<Income> Incomes { get; set; }
         public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
         public DbSet<Expense> Expenses { get; set; }
-
-        public string ConnectionString { get; set; }
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -38,6 +21,5 @@ namespace Persistence.Data
             modelBuilder.Entity<ExpenseCategory>().ToTable("ExpenseCategory");
             modelBuilder.Entity<Expense>().ToTable("Expense");
         }
-
     }
 }
