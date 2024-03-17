53197d8cac1c4ba9dbe49dd05c965b42aae6307b
c1own-depressed <dimayanishevsky@gmail.com>
2024-03-17 16:14:52 +0200

    1

---
+++ ManageYourBudget/WebApp/Controllers/SettingsPageContorller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly ILogger<SettingsController> _logger;
+
+        public SettingsController(ILogger<SettingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Views/SettingsPage/Index.cshtml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
