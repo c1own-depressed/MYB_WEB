706b029b469bcae51fc4d318a604620564117ad5
c1own-depressed <dimayanishevsky@gmail.com>
2024-04-11 21:03:06 +0300

    Saved

--- ManageYourBudget/Application/Interfaces/IStatisticService.cs
+++ ManageYourBudget/Application/Interfaces/IStatisticService.cs
@@ -6,6 +6,7 @@ namespace Application.Interfaces
 
         Task<IEnumerable<IncomeStatisticDTO>> getIncomesByDate(DateTime startMonth, DateTime endMonth, int UserId);
         Task<IEnumerable<TotalExpensesDTO>> GetTotalExpensesByDate(DateTime from, DateTime to, int userId);
+        Task<IEnumerable<SavedStatisticDTO>> CountSaved(DateTime from, DateTime to, int userId);
 
     }
 }
