f1a59715089aba95d624c11f1f7f9b60717a5edf
Javenhun733r <andrij.babyuk@gmail.com>
2024-04-11 19:40:41 +0300

    Created an income statistic

--- ManageYourBudget/Application/Interfaces/IStatisticService.cs
+++ ManageYourBudget/Application/Interfaces/IStatisticService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.DTOs.StatisticDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Application.Interfaces
 {
     public interface IStatisticService
     {
+        Task<IEnumerable<IncomeStatisticDTO>> getIncomesByDate(DateTime startMonth, DateTime endMonth, int UserId);
     }
 }
--- ManageYourBudget/Application/Services/StatisticService.cs
+++ ManageYourBudget/Application/Services/StatisticService.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Application.DTOs.StatisticDTO;
+using Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.DTOs.IncomeDTOs;
+using Domain.Interfaces;
+using Application.Utils;
+using Domain.Entities;
 
 namespace Application.Services
 {
-    public class StatisticService
+    public class StatisticService : IStatisticService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public async Task<IEnumerable<IncomeStatisticDTO>> getIncomesByDate (DateTime startDate, DateTime endDate, int UserId)
+        {
+            var user = await this._unitOfWork.Users.GetByIdAsync(UserId);
+            var incomes = await this._unitOfWork.Incomes.GetIncomesByUserIdAsync(UserId);
+
+            string currencyRepresentation = CurrencyUtils.FormatCurrencyDisplay(user.Currency);
+
+            var incomeDTOs = incomes
+                .Where(income => (income.Date >= startDate && income.Date <= endDate) || income.IsRegular)
+                .Select(income => new IncomeDTO
+                {
+                    Id = income.Id,
+                    IncomeName = income.IncomeName,
+                    Amount = income.Amount,
+                    CurrencyEmblem = currencyRepresentation,
+                    Date = income.Date,
+                    IsRegular = income.IsRegular,
+                });
+            incomeDTOs.GroupBy(x => x.Date.Month);
+            var incomeByMonth = incomeDTOs
+                .GroupBy(income => new { Year = income.Date.Year, Month = income.Date.Month })
+                .Select(group => new IncomeStatisticDTO
+                {
+                    Month = new DateTime(group.Key.Year, group.Key.Month, 1),
+                    TotalAmount = group.Sum(income => income.Amount),
+                })
+                .OrderBy(dto => dto.Month);
+            return incomeByMonth;
+        }
     }
 }
