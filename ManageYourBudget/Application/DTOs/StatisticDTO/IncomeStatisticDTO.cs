a893b4706f677d1a6f17cb14a91b415f7da8df8c
Javenhun733r <andrij.babyuk@gmail.com>
2024-04-11 19:09:43 +0300

    Added a statistic structure

---
+++ ManageYourBudget/Application/DTOs/StatisticDTO/IncomeStatisticDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.StatisticDTO
+{
+    internal class IncomeStatisticDTO
+    {
+        public DateTime Month { get; set; }
+
+        public int TotalAmount { get; set; }
+    }
+}
