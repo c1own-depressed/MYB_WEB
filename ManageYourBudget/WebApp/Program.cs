dbcd69304c3b33c2393e4e5b804df05515c5a491
Rednigo <barostikq@gmail.com>
2024-04-26 13:00:38 +0300

    Updated Account and FAQPage to localize

--- ManageYourBudget/WebApp/Program.cs
+++ ManageYourBudget/WebApp/Program.cs
@@ -18,7 +18,7 @@ builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console()
     .WriteTo.Seq("http://localhost:5341"));
 
-var connectionString = builder.Configuration.GetConnectionString("DimaConnection");
+var connectionString = builder.Configuration.GetConnectionString("RostikConnection");
 
 
 if (connectionString != null)
